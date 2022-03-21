6281016be9c53af36440625093fc448069b7a0f9
MTiunchik <MTiunchik@inno.tech>
2022-03-21 09:58:24 +0300

    lesson examples

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,81 +1,109 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Button
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.drawscope.DrawScope
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.window.Tray
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
-import kotlinx.coroutines.delay
 
 /**
  * https://github.com/JetBrains/compose-jb/tree/master/tutorials
  */
 
 @Composable
-@Preview
-fun App() {
-    var text by remember { mutableStateOf("Hello, World!") }
-    var count = remember { mutableStateOf(0) }
-    LaunchedEffect(Unit) {
-        while (true) {
-            count.value++
-            delay(1000)
-        } 
+@Preview()
+fun previewMessageCard() {
+    MaterialTheme(
+//        colors = MaterialTheme.colors.copy(background = Color.Black, primaryVariant = Color.White)
+    ) {
+        conversation(SampleData.conversationSample)
     }
-    MaterialTheme {
-        
-        Row(Modifier.fillMaxSize(), Arrangement.Center,  Alignment.CenterVertically) {
-            Button(onClick = {
-                text = "Hello, Desktop!"
-            }) {
-                Text(text)
+}
+
+@Composable
+fun conversation(messages: List<Message>){
+    LazyColumn {
+        items(messages) { messageCard(it)}
+    }
+}
+
+@Composable
+fun messageCard(msg: Message) {
+    Row(modifier = Modifier
+        .padding(all = 8.dp)
+        .background(MaterialTheme.colors.background)
+    ) {
+        Image(
+            painter = painterResource("img.png"),
+            contentDescription = "Contact profile picture",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
+        )
+
+        Spacer(modifier = Modifier.width(8.dp))
+
+        var isExpanded by remember { mutableStateOf(false) }
+        val surfaceColor: Color by animateColorAsState(
+            if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.secondary
+        )
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colors.secondaryVariant,
+                style = MaterialTheme.typography.subtitle2
+            )
+            Spacer(modifier = Modifier.width(4.dp))
+
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                elevation = 3.dp,
+                color = surfaceColor,
+                modifier = Modifier.animateContentSize().padding(1.dp)
+            ) {
+                Text(
+                    text = msg.body,
+                    color = MaterialTheme.colors.primaryVariant,
+                    modifier = Modifier.padding(all = 4.dp),
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                    style = MaterialTheme.typography.body2
+                )
             }
-            Button(onClick = {
-                count.value++
-            }) {
-                Text("Increase ${count.value}")
-            }
+
         }
     }
+
+
 }
+
+data class Message(val author: String, val body: String)
+
 
 fun main() = application {
-    var isVisible by remember { mutableStateOf(true) }
     Window(
-        onCloseRequest = { isVisible = false },
-        visible = isVisible,
-        title = "My first App"
+        state = WindowState(size = DpSize(800.dp, 300.dp)),
+        onCloseRequest = ::exitApplication,
     ) {
-        App()
-    }
-    if (!isVisible) {
-        Tray(
-            icon = TryIcon,
-            tooltip = "TestApp",
-            onAction = { isVisible = true },
-            menu = {
-                Item("Show", onClick = { isVisible = true})
-                Item("Exit", onClick = ::exitApplication)
-            }
-        )
+        previewMessageCard()
     }
 }
 
-object TryIcon : Painter() {
-    override val intrinsicSize: Size = Size(256f, 256f)
-
-    override fun DrawScope.onDraw() {
-        drawRect(Color.Red)
-    }
-}
