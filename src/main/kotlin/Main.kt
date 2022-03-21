a85782cbf2e89b5e5305494125fbf964fca6d67a
MTiunchik <MTiunchik@inno.tech>
2022-03-21 23:10:37 +0300

    lesson examples 2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,10 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -29,7 +27,7 @@ import kotlinx.coroutines.delay
 
 fun main() = application {
     Window(
-        onCloseRequest =  { this.exitApplication() }
+        onCloseRequest = { this.exitApplication() }
     ) {
         App()
     }
@@ -44,11 +42,26 @@ fun App() {
 }
 
 @Composable
-fun Element(){
-    Surface(
-        color = MaterialTheme.colors.primary,
-    elevation = 4.dp) {
-        Greetings("Windows")
+fun Element() {
+    var newName by remember { mutableStateOf("none") }
+    Column {
+        Surface(
+            color = MaterialTheme.colors.primary,
+            elevation = 4.dp,
+            shape = MaterialTheme.shapes.medium
+        ) {
+            Greetings(newName)
+        }
+        TextField(
+            value = newName,
+            onValueChange = { newName = it })
+    }
+}
+
+@Composable
+fun ManyElements() {
+    Column {
+
     }
 }
 
