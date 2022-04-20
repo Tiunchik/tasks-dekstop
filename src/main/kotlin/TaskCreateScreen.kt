8ebe16b25280d5d04de084003db6e55402fb0d6c
MTiunchik <MTiunchik@inno.tech>
2022-04-21 00:03:13 +0300

    stable task creation

--- src/main/kotlin/TaskCreateScreen.kt
+++ src/main/kotlin/TaskCreateScreen.kt
@@ -1,22 +1,32 @@
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Send
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.*
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.arkivanov.decompose.router.Router
 import com.arkivanov.decompose.router.pop
 import config.decompose.Pages
+import models.Task
 import org.koin.java.KoinJavaComponent
 import retrofit.TaskController
+import retrofit2.awaitResponse
 
 @Composable
 fun TaskCreateScreen(router: Router<Pages, Any>) {
-    val controller by KoinJavaComponent.inject<TaskController>(TaskController::class.java)
 
+    val controller by KoinJavaComponent.inject<TaskController>(TaskController::class.java)
     var name by remember { mutableStateOf("") }
     var description by remember { mutableStateOf("") }
+    var blockedButton by remember { mutableStateOf(true) }
 
     Column {
         TopAppBar(
@@ -28,15 +38,29 @@ fun TaskCreateScreen(router: Router<Pages, Any>) {
                         contentDescription = null
                     )
                 }
+                IconButton(onClick = {
+                    blockedButton = false
+                    controller.createTask(Task(name = name, description = description)).execute()
+                    name = ""
+                    description = ""
+                    blockedButton = true
+                }, enabled = blockedButton) {
+                    Icon(
+                        imageVector = Icons.Default.Send,
+                        contentDescription = "Send request for creating task"
+                    )
+                }
             }
         )
         TextField(
             value = name,
             onValueChange = { name = it }
         )
-        TextField(
+
+        BasicTextField(
             value = description,
-            onValueChange = { description = it }
+            onValueChange = { description = it },
+            modifier = Modifier.fillMaxSize().border(2.dp, color = Color.Black)
         )
     }
 }
--- src/main/kotlin/retrofit/TaskController.kt
+++ src/main/kotlin/retrofit/TaskController.kt
@@ -2,7 +2,9 @@ package retrofit
 
 import models.Task
 import retrofit2.Call
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface TaskController {
@@ -12,5 +14,8 @@ interface TaskController {
 
     @GET("/api/v1/tasks/{id}")
     fun getOneTask(@Path("id") id: Long): Call<Task>
+
+    @POST("/api/v1/tasks")
+    fun createTask(@Body task: Task): Call<Task>
 
 }
